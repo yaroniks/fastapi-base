123a1a8f62e9ef6ff038c9c0f9d0bd6456e800cc
yarovich <anokhin247yarik0@gmail.com>
2025-03-22 16:40:58 +0300

    добавлен ErrorMessage

--- app/common/schemas/base.py
+++ app/common/schemas/base.py
@@ -1,7 +1,10 @@
-from typing import Optional
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel
 
 
 class Response(BaseModel):
     success: bool
     message: str
+
+
+class ErrorMessage(BaseModel):
+    detail: str
