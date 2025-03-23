8c9a39cd0c1e9249e40910c55adb0b2c3d77587e
yarovich <anokhin247yarik0@gmail.com>
2025-03-23 12:32:07 +0300

    добавлен rabbitmq, пример роутера

--- app/routers/example/router.py
+++ app/routers/example/router.py
@@ -3,3 +3,9 @@ import app.common.schemas as schemas
 from fastapi import APIRouter, Request, Response, HTTPException, status
 
 router = APIRouter(prefix='/example', tags=['Example'])
+
+
+@router.get('/', summary='Пример', response_model=schemas.Response,
+            responses={404: {'model': schemas.ErrorMessage}})
+async def get_example():
+    return {'success': True, 'message': 'Success.'}
