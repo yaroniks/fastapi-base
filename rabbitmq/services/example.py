90e21ebdfc3f16b147fad000244923737670d32e
yarovich <anokhin247yarik0@gmail.com>
2025-03-23 19:55:00 +0300

    добавлен Dockerfile и переименовал req.txt

--- rabbitmq/services/example.py
+++ rabbitmq/services/example.py
@@ -5,7 +5,7 @@ from config import settings
 class RabbitMQExample:
     _instance = None
     _initialized = False
-    __queue = 'example'
+    _queue = 'example'
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -13,8 +13,6 @@ class RabbitMQExample:
         return cls._instance
 
     def __init__(self):
-        # Так как в ините создаём соединение, мы это должны делать только один раз
-        # Крч похуй, просто скопировал код
         if self.__class__._initialized:
             return
         self.__class__._initialized = True
@@ -23,18 +21,18 @@ class RabbitMQExample:
             pika.ConnectionParameters(host=settings.RABITMQ_HOST, port=settings.RABITMQ_PORT)
         )
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.__queue)
+        self.channel.queue_declare(queue=self._queue)
 
     async def worker(self) -> None:
         def callback(ch, method, properties, body):
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
         while True:
-            self.channel.basic_consume(queue=self.__queue, on_message_callback=callback, auto_ack=True)
+            self.channel.basic_consume(queue=self._queue, on_message_callback=callback, auto_ack=True)
             self.channel.start_consuming()
 
     async def send(self, message) -> None:
-        self.channel.basic_publish(exchange='', routing_key=self.__queue, body=message)
+        self.channel.basic_publish(exchange='', routing_key=self._queue, body=message)
 
     async def close_rabbitmq(self) -> None:
         self.connection.close()
