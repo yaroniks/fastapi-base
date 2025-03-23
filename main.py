45be9a1e975cb3c632872318a447067a368406b7
yarovich <anokhin247yarik0@gmail.com>
2025-03-23 20:06:47 +0300

    добавлен limiter

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from app.routers import *
+from app.limiter import *
 from config import settings
 import app.database.requests as req
 import app.common.schemas as schemas
+from contextlib import asynccontextmanager
 
 import uvicorn
-from contextlib import asynccontextmanager
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request, Response, HTTPException, status
 from starlette.responses import RedirectResponse, JSONResponse, HTMLResponse
@@ -26,9 +29,12 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=['*'],
                    allow_headers=['*'],
                    allow_credentials=True)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.state.limiter = limiter
 
 
 @app.get('/', summary='Документация', tags=['Docs'], response_class=HTMLResponse)
+@limiter.limit('60/minute')
 async def home(request: Request, response: Response):
     return RedirectResponse('/api/v1/docs/')
 
