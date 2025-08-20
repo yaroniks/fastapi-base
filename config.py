a4c096d9618f7219f756e8162b9c320aebdb6602
yarovich <anokhin247yarik0@gmail.com>
2025-08-21 00:18:05 +0300

    tests

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(env_file=str(Path(__file__).parent) + '\.env', env_file_encoding='utf-8', extra='ignore')
 
     TITLE: str
     VERSION: str
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,5 +5,11 @@ client = TestClient(app)
 
 
 def test_home():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
+
+
+def test_example():
+    response = client.get('/example')
+    data = response.json()
+    assert data['success'] is True
