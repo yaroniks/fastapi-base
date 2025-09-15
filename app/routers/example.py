3c4102f260843a6c08688a99af99e48b145e89e1
yarovich <anokhin247yarik0@gmail.com>
2025-09-15 16:25:31 +0300

    add example fileupload and test

--- app/routers/example.py
+++ app/routers/example.py
@@ -1,7 +1,7 @@
 import app.schemas as schemas
 from app.limiter import limiter
 from app.database.models import *
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, UploadFile, Form, File
 
 router = APIRouter(prefix='/example', tags=['Example'])
 
@@ -11,3 +11,14 @@ router = APIRouter(prefix='/example', tags=['Example'])
 @limiter.limit('60/minute')
 async def get_example(request: Request):
     return {'success': True, 'message': 'Success.'}
+
+
+@router.post('/upload', summary='Пример загрузки файла',
+             responses={429: {'model': schemas.ErrorMessage}})
+@limiter.limit('60/minute')
+async def upload_file(
+        request: Request,
+        name: str = Form(),
+        file: UploadFile = File()
+):
+    return {"filename": name}
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,9 @@ def test_example():
     response = client.get('/example')
     data = response.json()
     assert data['success'] is True
+
+
+def test_upload():
+    response = client.post('/example/upload', data={'name': 'string'}, files={'file': 'test.txt'})
+    data = response.json()
+    assert data['filename'] == 'string'
