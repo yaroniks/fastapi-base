089826977d281b151270c596619e03bd924081d1
yarovich <anokhin247yarik0@gmail.com>
2026-06-21 21:09:33 +0300

    update some staff

--- app/limiter.py
+++ app/limiter.py
@@ -3,9 +3,7 @@ from config import settings
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
-
-# limiter = Limiter(
-#     key_func=get_remote_address,
-#     storage_uri=f'redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0'
-# )
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri=f'redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0'
+)
